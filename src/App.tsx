3b143c8cdb027b89584b2318439d63cfb46dd141
Tyler S <42685071+Tyler-Seliber@users.noreply.github.com>
2023-02-11 22:30:37 -0500

    schedule view with pre-filled courses

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel } from '@syncfusion/ej2-react-schedule';
+import { appointments } from './courses/selected_courses';
 
 function App() {
+  var localData: EventSettingsModel = { dataSource: appointments };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ScheduleComponent currentView='WorkWeek' selectedDate={new Date(2023, 0, 1)} eventSettings={localData}>
+          <Inject services={[Week, WorkWeek, Agenda]} />
+        </ScheduleComponent>
       </header>
     </div>
   );
---
+++ src/courses/selected_courses.js
@@ -0,0 +1,43 @@
+export const appointments = [
+    {
+        StartTime: new Date(2023, 0, 2, 12, 0),
+        EndTime: new Date(2023, 0, 2, 13, 50),
+        Subject: 'CAL 105 A - Knowledge, Nature, Culture',
+        Location: 'M 103',
+        Description: 'Instructor: Nicolosi, Antonio',
+        RecurrenceRule: 'FREQ=WEEKLY;BYDAY=MO,WE,FR'
+    },
+    {
+        StartTime: new Date(2023, 0, 2, 9, 0),
+        EndTime: new Date(2023, 0, 2, 9, 50),
+        Subject: 'MA 125 B - Vectors and Series',
+        Location: 'BC 321',
+        Description: 'Instructor: Miller, Patrick',
+        RecurrenceRule: 'FREQ=WEEKLY;BYDAY=MO,WE,FR'
+    },
+    {
+        StartTime: new Date(2023, 0, 3, 10, 0),
+        EndTime: new Date(2023, 0, 3, 10, 50),
+        Subject: 'MA 125 RE - Vectors and Series',
+        Location: 'B 715',
+        Description: 'Instructor: Miller, Patrick',
+        RecurrenceRule: 'FREQ=WEEKLY;BYDAY=TU,TH'
+    },
+    {
+        StartTime: new Date(2023, 0, 2, 15, 0),
+        EndTime: new Date(2023, 0, 2, 15, 50),
+        Subject: 'CH 116 D - General Chemistry II',
+        Location: 'EAS 222',
+        Description: 'Instructor: Muisener, Patricia',
+        RecurrenceRule: 'FREQ=WEEKLY;BYDAY=MO,WE,FR'
+    },
+    {
+        StartTime: new Date(2023, 0, 5, 11, 0),
+        EndTime: new Date(2023, 0, 5, 14, 50),
+        Subject: 'CH 118 S - General Chemistry II Lab',
+        Location: 'X 204',
+        Description: 'Instructor: Kim, Faith',
+        RecurrenceRule: 'FREQ=WEEKLY;BYDAY=TH'
+    }
+    
+]
